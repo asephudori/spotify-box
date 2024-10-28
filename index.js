abcd8468d2b88702d36b7ce25afe1f66c33b4032
Asep Hudori <88381571+asephudori@users.noreply.github.com>
2024-10-28 15:49:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function updateTopTracks(json) {
       gist_id,
       files: {
         [filename]: {
-          filename: '🎵 My Spotify Top Tracks of the Week',
+          filename: '🎵 Spotify Tracks of the Week',
           content: lines.join('\n'),
         },
       },
